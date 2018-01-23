5e2585b73a70589e0544823fc8834795925da9eb
Brandon Stewart <boveus@gmail.com>
2018-01-23 13:54:41 -0500

    iteration 0 done with tests

--- test/output_test.rb
+++ test/output_test.rb
@@ -1,23 +1,16 @@
 require 'minitest'
 require 'minitest/autorun'
-require './lib/output'
+require './lib/runner'
 require 'net/http'
 require "uri"
 
 class OutputTest < Minitest::Test
-  def test_it_can_make_request
-    skip
-    output = Output.new
-    output.start_server
-    assert_equal 9, output.request_lines.length
-    assert_equal 1, output.counter
-    output.stop_server
-  end
-
   def test_that_it_increments
-    output = Output.new
-    output.start_server
-    assert_equal 2, output.counter
-    output.stop_server
+    uri = URI.parse("http://localhost:9292")
+    Net::HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri)
+
+    assert_equal response.body, "<html><head></head><body>Hello, World! (2)</body></html>"
+    Runner.new.stop
   end
 end
