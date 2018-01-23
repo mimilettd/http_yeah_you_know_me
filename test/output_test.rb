2d6b3f964ab71f8076868dcd9535601e7ed1cb83
mimilettd <mimilettd@gmail.com>
2018-01-23 11:36:05 -0700

    test that it increments

--- test/output_test.rb
+++ test/output_test.rb
@@ -6,8 +6,18 @@ require "uri"
 
 class OutputTest < Minitest::Test
   def test_it_can_make_request
+    skip
     output = Output.new
-    output.make_request
+    output.start_server
     assert_equal 9, output.request_lines.length
+    assert_equal 1, output.counter
+    output.stop_server
+  end
+
+  def test_that_it_increments
+    output = Output.new
+    output.start_server
+    assert_equal 2, output.counter
+    output.stop_server
   end
 end
