24195c24f3d8ab73a14634b20e163924443cbd06
mimilettd <mimilettd@gmail.com>
2018-01-23 10:42:56 -0700

    test that it can make a response

--- test/output_test.rb
+++ test/output_test.rb
@@ -1,1 +1,13 @@
-# Test
+require 'minitest'
+require 'minitest/autorun'
+require './lib/output'
+require 'net/http'
+require "uri"
+
+class OutputTest < Minitest::Test
+  def test_it_can_make_request
+    output = Output.new
+    output.make_request
+    assert_equal 9, output.request_lines.length
+  end
+end
